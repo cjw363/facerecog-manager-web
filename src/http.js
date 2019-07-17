5b9e3cf1ae5e4d94aef17ecad9df81fe6a2bb122
cjw363 <asd6555363>
2019-07-17 16:28:30 +0800

    提交

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:9090/' // 设置axios的基础请求路径
+axios.defaults.baseURL = 'http://127.0.0.1:9090/cpfr/' // 设置axios的基础请求路径
 // axios.defaults.baseURL = 'http://132.232.108.138/cpfr' // 设置axios的基础请求路径
 axios.defaults.timeout = 2000 // 设置axios的请求时间
 
